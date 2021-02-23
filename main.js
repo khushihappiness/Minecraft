9aebaa46bbfde81113a2742da4750bf713892031
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-02-23 15:30:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -136,3 +136,50 @@ console.log("right");
 }
 }
 
+function up()
+{
+	if(player_y >=0)
+	{
+		player_y = player_y - block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("When Up arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		playerUpdate();
+	}
+}
+
+function down()
+{
+	if(player_y <=500)
+	{
+		player_y = player_y + block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("When Down arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		playerUpdate();
+	}
+}
+
+function left()
+{
+	if(player_x >=0)
+	{
+		player_x = player_x - block_image_width;
+		console.log("block image width = " + block_image_width);
+		console.log("When Left arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		playerUpdate();
+	}
+}
+
+function right()
+{
+	if(player_x <=850)
+	{
+		player_x = player_x + block_image_width;
+		console.log("block image width = " + block_image_width);
+		console.log("When Right arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		playerUpdate();
+	}
+}
